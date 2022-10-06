d558f6f3981df24f3902f3a47bba372e9434c235
Takeshi Nakatani <nakatani@yahoo-corp.jp>
2022-10-06 09:30:10 +0900

    Updated to support common autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -24,36 +24,47 @@
 # REVISION:
 #
 
+#
+# Common variables
+#
+AUTOGEN_NAME=$(basename "${0}")
+AUTOGEN_DIR=$(dirname "${0}")
+SRCTOP=$(cd "${AUTOGEN_DIR}" || exit 1; pwd)
 
 #
-# Usage: autogen.sh [-noupdate_version_file] [-no_aclocal_force] [-no_check_ver_diff]
+# Run
 #
-AUTOGEN_NAME=`basename $0`
-AUTOGEN_DIR=`dirname $0`
-SRCTOP=`cd ${AUTOGEN_DIR}; pwd`
-
-echo "** run autogen.sh"
+echo "[RUN] autogen.sh"
+echo ""
 
 #
-# Parameter
+# Parse parameters
 #
-NOUPDATE="no"
+UPDATE_VERSION_FILE=1
 FORCEPARAM="--force"
 PARAMETERS=""
 while [ $# -ne 0 ]; do
-	if [ "X$1" = "X-noupdate_version_file" ]; then
-		NOUPDATE="yes"
-		FORCEPARAM=""	# do not need force
-	elif [ "X$1" = "X-no_aclocal_force" ]; then
-		FORCEPARAM=""
-	elif [ "X$1" = "X-no_check_ver_diff" ]; then
-		PARAMETERS="${PARAMETERS} $1"
-	elif [ "X$1" = "X-h" -o "X$1" = "X--help" ]; then
-		echo "Usage: ${AUTOGEN_NAME} [-noupdate_version_file] [-no_aclocal_force] [-no_check_ver_diff]"
+	if [ -z "$1" ]; then
+		echo "[ERROR] Parameters are something wrong."
 		exit 1
+
+	elif [ "$1" = "-h" ] || [ "$1" = "-H" ] || [ "$1" = "--help" ] || [ "$1" = "--HELP" ]; then
+		echo "Usage: ${AUTOGEN_NAME} [--help(-h)] [--no_update_version_file(-nu)] [--no_aclocal_force(-na)] [--no_check_ver_diff(-nc)]"
+		exit 0
+
+	elif [ "$1" = "-nu" ] || [ "$1" = "-NU" ] || [ "$1" = "--no_update_version_file" ] || [ "$1" = "--NO_UPDATE_VERSION_FILE" ]; then
+		UPDATE_VERSION_FILE=0
+		FORCEPARAM=""			# do not need force
+
+	elif [ "$1" = "-na" ] || [ "$1" = "-NA" ] || [ "$1" = "--no_aclocal_force" ] || [ "$1" = "--NO_ACLOCAL_FORCE" ]; then
+		FORCEPARAM=""
+
+	elif [ "$1" = "-nc" ] || [ "$1" = "-NC" ] || [ "$1" = "--no_check_ver_diff" ] || [ "$1" = "--NO_CHECK_VER_DIFF" ]; then
+		PARAMETERS="$1"
+
 	else
-		echo "ERROR: Unknown option $1"
-		echo "Usage: ${AUTOGEN_NAME} [-noupdate_version_file] [-no_aclocal_force] [-no_check_ver_diff]"
+		echo "[ERROR] Unknown option $1"
+		echo "Usage: ${AUTOGEN_NAME} [--help(-h)] [--no_update_version_file(-nu)] [--no_aclocal_force(-na)] [--no_check_ver_diff(-nc)]"
 		exit 1
 	fi
 	shift
@@ -62,11 +73,10 @@ done
 #
 # update RELEASE_VERSION file
 #
-if [ "X${NOUPDATE}" = "Xno" ]; then
-	echo "--- run make_release_version_file.sh"
-	${SRCTOP}/buildutils/make_release_version_file.sh ${PARAMETERS}
-	if [ $? -ne 0 ]; then
-		echo "ERROR: update RELEASE_VERSION file"
+if [ "${UPDATE_VERSION_FILE}" -eq 1 ] && [ -f "${SRCTOP}/buildutils/make_release_version_file.sh" ]; then
+	echo "    [INFO] run make_release_version_file.sh"
+	if ! /bin/sh -c "${SRCTOP}/buildutils/make_release_version_file.sh" "${PARAMETERS}" 2>&1 | sed -e 's|^|        |g'; then
+		echo "[ERROR] update RELEASE_VERSION file"
 		exit 1
 	fi
 fi
@@ -74,27 +84,26 @@ fi
 #
 # Check files
 #
-if [ ! -f ${SRCTOP}/NEWS ]; then
-	touch ${SRCTOP}/NEWS
+if [ ! -f "${SRCTOP}/NEWS" ]; then
+	touch "${SRCTOP}/NEWS"
 fi
-if [ ! -f ${SRCTOP}/README ]; then
-	touch ${SRCTOP}/README
+if [ ! -f "${SRCTOP}/README" ]; then
+	touch "${SRCTOP}/README"
 fi
-if [ ! -f ${SRCTOP}/AUTHORS ]; then
-	touch ${SRCTOP}/AUTHORS
+if [ ! -f "${SRCTOP}/AUTHORS" ]; then
+	touch "${SRCTOP}/AUTHORS"
 fi
-if [ ! -f ${SRCTOP}/ChangeLog ]; then
-	touch ${SRCTOP}/ChangeLog
+if [ ! -f "${SRCTOP}/ChangeLog" ]; then
+	touch "${SRCTOP}/ChangeLog"
 fi
 
 #
 # Auto scan
 #
-if [ ! -f configure.scan -o "X${FORCEPARAM}" != "X" ]; then
-	echo "--- run autoscan"
-	autoscan
-	if [ $? -ne 0 ]; then
-		echo "ERROR: something error occurred in autoscan"
+if [ ! -f configure.scan ] || [ -n "${FORCEPARAM}" ]; then
+	echo "    [INFO] run autoscan"
+	if ! autoscan 2>&1 | sed -e 's|^|        |g'; then
+		echo "[ERROR] something error occurred in autoscan"
 		exit 1
 	fi
 fi
@@ -102,48 +111,54 @@ fi
 #
 # Copy libtools
 #
-libtoolize --force --copy
-if [ $? -ne 0 ]; then
-	echo "ERROR: something error occurred in libtoolize"
-	exit 1
+if grep -q 'LT_INIT' configure.ac configure.scan; then
+	if ! libtoolize --force --copy 2>&1 | sed -e 's|^|        |g'; then
+		echo "[ERROR] something error occurred in libtoolize"
+		exit 1
+	fi
 fi
 
 #
 # Build configure and Makefile
 #
-echo "--- run aclocal ${FORCEPARAM}"
-aclocal ${FORCEPARAM}
-if [ $? -ne 0 ]; then
-	echo "ERROR: something error occurred in aclocal ${FORCEPARAM}"
+echo "    [INFO] run aclocal ${FORCEPARAM}"
+if ! /bin/sh -c "aclocal ${FORCEPARAM}" 2>&1 | sed -e 's|^|        |g'; then
+	echo "[ERROR] something error occurred in aclocal ${FORCEPARAM}"
 	exit 1
 fi
 
-echo "--- run autoheader"
-autoheader
-if [ $? -ne 0 ]; then
-	echo "ERROR: something error occurred in autoheader"
+if grep -q 'AC_CONFIG_HEADERS' configure.ac configure.scan; then
+	echo "    [INFO] run autoheader"
+	if ! autoheader 2>&1 | sed -e 's|^|        |g'; then
+		echo "[ERROR] something error occurred in autoheader"
+		exit 1
+	fi
+fi
+
+echo "    [INFO] run automake -c --add-missing"
+if ! automake -c --add-missing 2>&1 | sed -e 's|^|        |g'; then
+	echo "[ERROR] something error occurred in automake -c --add-missing"
 	exit 1
 fi
 
-echo "--- run automake -c --add-missing"
-automake -c --add-missing
-if [ $? -ne 0 ]; then
-	echo "ERROR: something error occurred in automake -c --add-missing"
+echo "    [INFO] run autoconf"
+if ! autoconf 2>&1 | sed -e 's|^|        |g'; then
+	echo "[ERROR] something error occurred in autoconf"
 	exit 1
 fi
 
-echo "--- run autoconf"
-autoconf
-if [ $? -ne 0 ]; then
-	echo "ERROR: something error occurred in autoconf"
-	exit 1
-fi
-
-echo "** SUCCEED: autogen"
+#
+# Finish
+#
+echo ""
+echo "[SUCCEED] autogen.sh"
 exit 0
 
 #
-# VIM modelines
-#
-# vim:set ts=4 fenc=utf-8:
+# Local variables:
+# tab-width: 4
+# c-basic-offset: 4
+# End:
+# vim600: noexpandtab sw=4 ts=4 fdm=marker
+# vim<600: noexpandtab sw=4 ts=4
 #
