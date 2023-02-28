7405e1e0cece45d9aab868657cb0c25566d177e0
Takeshi Nakatani <nakatani@yahoo-corp.jp>
2023-02-28 17:07:15 +0900

    Updated build helper scripts on CI process

--- buildutils/debian_build.sh
+++ buildutils/debian_build.sh
@@ -19,7 +19,7 @@
 #
 # AUTHOR:   Takeshi Nakatani
 # CREATE:   Fri, Apr 13 2018
-# REVISION:
+# REVISION: 1.1
 #
 
 #==============================================================
@@ -33,16 +33,18 @@ PIPEFAILURE_FILE="/tmp/.pipefailure.$(od -An -tu4 -N4 /dev/random | tr -d ' \n')
 #
 # For shellcheck
 #
-if locale -a | grep -q -i '^[[:space:]]*C.utf8[[:space:]]*$'; then
-	LANG=$(locale -a | grep -i '^[[:space:]]*C.utf8[[:space:]]*$' | sed -e 's/^[[:space:]]*//g' -e 's/[[:space:]]*$//g' | tr -d '\n')
-	LC_ALL="${LANG}"
-	export LANG
-	export LC_ALL
-elif locale -a | grep -q -i '^[[:space:]]*en_US.utf8[[:space:]]*$'; then
-	LANG=$(locale -a | grep -i '^[[:space:]]*en_US.utf8[[:space:]]*$' | sed -e 's/^[[:space:]]*//g' -e 's/[[:space:]]*$//g' | tr -d '\n')
-	LC_ALL="${LANG}"
-	export LANG
-	export LC_ALL
+if command -v locale >/dev/null 2>&1; then
+	if locale -a | grep -q -i '^[[:space:]]*C.utf8[[:space:]]*$'; then
+		LANG=$(locale -a | grep -i '^[[:space:]]*C.utf8[[:space:]]*$' | sed -e 's/^[[:space:]]*//g' -e 's/[[:space:]]*$//g' | tr -d '\n')
+		LC_ALL="${LANG}"
+		export LANG
+		export LC_ALL
+	elif locale -a | grep -q -i '^[[:space:]]*en_US.utf8[[:space:]]*$'; then
+		LANG=$(locale -a | grep -i '^[[:space:]]*en_US.utf8[[:space:]]*$' | sed -e 's/^[[:space:]]*//g' -e 's/[[:space:]]*$//g' | tr -d '\n')
+		LC_ALL="${LANG}"
+		export LANG
+		export LC_ALL
+	fi
 fi
 
 #
